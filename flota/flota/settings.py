ea12ade63ff98da9202559aedbbb7b582cadd6c8
Jesus Smith <elvispoo.2014@gmail.com>
2026-03-13 17:41:46 -0500

    login admin mejorado

--- flota/flota/settings.py
+++ flota/flota/settings.py
@@ -143,7 +143,7 @@ TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
 
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
 
         "APP_DIRS": True,
 
