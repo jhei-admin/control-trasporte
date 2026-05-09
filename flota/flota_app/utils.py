65a64be534b0b35f9046e82252d043a3016045c1
Jesus Smith <elvispoo.2014@gmail.com>
2026-05-09 12:08:51 -0500

    Eliminar tolerancia antigua de utils

--- flota/flota_app/utils.py
+++ flota/flota_app/utils.py
@@ -75,28 +75,3 @@ def obtener_salida_hoy(vehiculo):
         .first()
     )
 
-
-# =================================================
-# 📍 EVALUAR MARCACIÓN
-# =================================================
-def evaluar_marcacion(marcacion: MarcacionPunto, tolerancia_min=2):
-
-    if not marcacion.hora_programada or not marcacion.hora_marcada:
-        return
-
-    diferencia = marcacion.hora_marcada - marcacion.hora_programada
-    minutos = int(diferencia.total_seconds() / 60)
-
-    marcacion.diferencia_minutos = minutos
-
-    if minutos < -tolerancia_min:
-        marcacion.estado = "adelantado"
-    elif minutos > tolerancia_min:
-        marcacion.estado = "tarde"
-    else:
-        marcacion.estado = "a_tiempo"
-
-    marcacion.save(update_fields=[
-        "diferencia_minutos",
-        "estado"
-    ])
